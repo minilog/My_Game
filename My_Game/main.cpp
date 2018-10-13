e2e9017d4e7c93d55b26990361ee4cc4ce04b139
Nguyễn Văn Minh <16520746@gm.uit.edu.vn>
2018-10-14 03:32:10 +0700

    add some shit, and I need to sleep

--- My_Game/main.cpp
+++ My_Game/main.cpp
@@ -1,6 +1,14 @@
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <Windows.h>
 #include <iostream>
+#include <sstream>
+#include <dinput.h>
+#include <windowsx.h>
+
 using namespace std;
-int main()
-{
-	std:cout << "Hello mother fucker";
-}
+
+#define WIN_NAME L"Game DirectX Name"
+#define WIN_TITLE L"Game DirectX Title"
+#define FPS 60
+#define KEYBOARD_BUFFERD_SIZE 1024
