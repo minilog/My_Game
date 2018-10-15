24879d63c53896348deacb5509f981fc4080ea91
Nguyễn Văn Minh <16520746@gm.uit.edu.vn>
2018-10-15 12:30:36 +0700

    SAVE

--- My_Game/HinhAnh.h
+++ My_Game/HinhAnh.h
@@ -10,13 +10,36 @@ class HinhAnh
 {
 public:
 	HinhAnh() = default;
-	HinhAnh(const char* in_DuongDan, RECT in_HCN = RECT(), int in_ChieuRong = NULL,
+
+	HinhAnh(const char* in_DuongDan, RECT in_HCN = RECT(), int in_ChieuRong = NULL, 
 		int in_ChieuCao = NULL, D3DCOLOR in_MauNen = NULL);
+
 	~HinhAnh();
 
+	void Ve(D3DXVECTOR3 in_ViTri = D3DXVECTOR3(), RECT in_HCN = RECT(), D3DXVECTOR2 in_TiLe = D3DXVECTOR2(), 
+		D3DXVECTOR2 in_DoDoi = D3DXVECTOR2(), float in_GocXoay = 0, D3DXVECTOR2 in_TrungTamXoayHinh = D3DXVECTOR2(), 
+		D3DXCOLOR in_MauNen = D3DCOLOR_XRGB(255, 255, 255));
+
+	LPDIRECT3DTEXTURE9 NhanKetCauHinhAnh();
+
+	D3DXIMAGE_INFO NhanThongTinHinhAnh();
+
+
+
+	void ThietLapChieuRong(int in_ChieuRong);
+
+	int NhanChieuRong();
+
+	void ThietLapChieuCao(int in_ChieuCao);
+
+	int NhanChieuCao();
+
+
+
 protected:
-	void KhoiTaoVoiHinhAnh(const char* in_DuongDan, RECT in_HCN = RECT(), int in_ChieuRong = NULL,
+	void KhoiTaoVoiHinhAnh(const char* in_DuongDan, RECT in_HCN = RECT(), int in_ChieuRong = NULL, 
 		int in_ChieuCao = NULL, D3DCOLOR in_MauNen = NULL);
+
 	bool LaHCN(RECT in_HCN);
 
 protected:
@@ -35,6 +58,5 @@ protected:
 	D3DXVECTOR2             mTrungTamXoayHinh;
 	D3DXVECTOR2             mTiLe;
 	D3DXVECTOR2             mDoDoi;				// Dời hình ảnh theo vị trí hình ảnh + độ dời
-
 };
 
