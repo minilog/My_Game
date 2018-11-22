d17d17c88b9c2b9454609391aea15cf1d9b3e302
Nguyễn Văn Minh <16520746@gm.uit.edu.vn>
2018-11-22 08:26:13 +0700

    .

--- My_Game/XMan.cpp
+++ My_Game/XMan.cpp
@@ -629,8 +629,8 @@ void XMan::LoadHinhAnhVao()
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(157.0f, 105.0f), 26, 36, 0.045f, HCN(157, 181, 105, 141)));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(181.0f,	105.0f), 30, 38, 0.045f, HCN(181, 212, 105, 141)));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(213.0f,	105.0f), 36, 40, 0.045f, HCN(213, 247, 105, 141)));
-	lDSTTFrame.push_back(ThongTinFrame(Vec2(247.0f,	105.0f), 28, 36, 0.045f));
-	//lDSTTFrame.push_back(ThongTinFrame(Vec2(276.0f,	105.0f), 22, 36, 0.045f));
+	//lDSTTFrame.push_back(ThongTinFrame(Vec2(247.0f,	105.0f), 28, 40, 0.045f, HCN(247, 275, 105, 143)));
+	lDSTTFrame.push_back(ThongTinFrame(Vec2(276.0f,	105.0f), 22, 38, 0.045f, HCN(276, 298, 105, 141)));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(298.0f,	105.0f), 28, 36, 0.045f));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(327.0f,	105.0f), 30, 36, 0.045f));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(357.0f,	105.0f), 34, 36, 0.045f));
@@ -642,7 +642,8 @@ void XMan::LoadHinhAnhVao()
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(126.0f, 192.0f), 24, 34, 0.045f, HCN(126, 158, 192, 227)));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(158.0f, 193.0f), 30, 34, 0.045f, HCN(158, 193, 193, 227)));
 	//lDSTTFrame.push_back(ThongTinFrame(Vec2(194.0f, 194.0f), 36, 34, 0.045f, HCN(194, 232, 194, 227)));
-	lDSTTFrame.push_back(ThongTinFrame(Vec2(194.0f, 194.0f), 36, 34, 0.045f, HCN(194, 232, 194, 227)));
+	//lDSTTFrame.push_back(ThongTinFrame(Vec2(233.0f, 194.0f), 28, 34, 0.045f, HCN(233, 267, 194, 227)));
+	lDSTTFrame.push_back(ThongTinFrame(Vec2(267.0f, 193.0f), 22, 34, 0.045f, HCN(267, 298, 193, 227)));
 	mHH_Chay_Ban = new HoatHinh("Resources_X3/XMan/XMan.png", lDSTTFrame);
 
 
